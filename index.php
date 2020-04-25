bdd1d1e9e1d23b2ea425eefe8311169a9d7d56c3
Blueprint Marketing <blueprintmrk@users.noreply.github.com>
2020-04-25 03:15:04 -0400

    index.php

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
-<?php get_header(); ?>
-	<h1><?php global $nc_blog_home_tagline; if(isset($nc_blog_home_tagline)) echo $nc_blog_home_tagline; else echo 'Recent Blog Posts'; ?></h1>
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<?php nc_generate_post_summary(); ?>
-	<?php endwhile; ?>
-	<?php endif; ?>
-	<?php nextclient_pagination(); ?>
-<?php get_footer(); ?>
+<?php
+// Silence is golden.
